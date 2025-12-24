8b161e70eafd512847700b3a5324ebc3f9ab1a19
AKzPrisma <ahmadkurniaprisma@gmail.com>
2025-12-24 10:41:38 +0700

    setupproject

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
-import type { NextConfig } from "next";
+import type { Config } from "tailwindcss";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        //Dark Mode
+        dark: {
+          bg: "#020617",
+          surface: "#0f172a",
+          accent: "#7c3aed",
+          text: "#f8fafc",
+        },
+        //Light Mode
+        light: {
+          bg: "#ffffff",
+          surface: "#f1f5f9",
+          accent: "#6366f1",
+          text: "#1e293b",
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+      },
+    },
+  },
+  plugins: [],
 };
-
-export default nextConfig;
+export default config;
